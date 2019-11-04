b51a64eea2e19136a5e36e5511634c151c4a2148
mraasf <32578342+mraasf@users.noreply.github.com>
2019-11-04 17:11:03 -0300

    Add files via upload
    alterado 04/11

---
+++ segredoDoCofre.js
@@ -0,0 +1,37 @@
+function Enviar(){
+	var N = document.getElementById(posicoesID)
+	if(posicoes.value != ""){
+		alert('Dados nviados com sucesso');
+		
+	}
+	
+}
+/*
+
+let N, M;
+entrada N, M ;
+
+let barra = [];
+let movimentos = [];
+let i;
+
+for (i = 0  i <= N;i++) entrada barra[i];
+for (i = 0  i <= M;i++) entrada movimentos[i];
+
+let freq = [0,0,0,0,0,0,0,0,0,0];
+
+for (i = 1; i < M ; i++){
+	let pI  = movimentos[i];
+	let pF = movimentos[i+1];
+	if(i > 1 && pF > pI) pI++;
+	if(i > 1 && pF < pI) pI--;
+	if() {
+		for (let j = pI; j <=pF; j++) freq[barra[j]] += 1;
+			}else if (pF < pI){
+			for (let j = pI; j <=pF; j++) freq[barra[j]] += 1;				
+			}
+	
+}
+for ( i=0;i<=9;i++) {
+        escreve freq[i] ' '; //imprime  a sequancia que abre o cofre
+}   */
